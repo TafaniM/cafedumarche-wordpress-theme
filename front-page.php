64a93cf47489526c386569e5aa7128a7bf7468d6
TafaniM <marina.tafani@studentfr.ch>
2024-05-22 14:20:09 +0200

    retour a 0

--- front-page.php
+++ front-page.php
@@ -1,8 +1,20 @@
 <?php
 /**
- * The template for displaying the front page.
+ * The template for displaying all pages.
  *
- * This is the template that displays the front page by default.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * To generate specific templates for your pages you can use:
+ * /mytheme/templates/page-mypage.twig
+ * (which will still route through this PHP file)
+ * OR
+ * /mytheme/page-mypage.php
+ * (in which case you'll want to duplicate this file and save to the above path)
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
  *
  * @package  WordPress
  * @subpackage  Timber
@@ -13,18 +25,4 @@ $context = Timber::context();
 
 $timber_post     = new Timber\Post();
 $context['post'] = $timber_post;
-
-// Vérifier si la page actuelle est la page d'accueil
-$context['is_front_page'] = is_front_page();
-
-// Si c'est la page d'accueil, récupérer le contenu du carousel
-if ($context['is_front_page']) {
-    ob_start();
-    include 'carousel.php'; // Inclure le fichier carousel.php
-    $carousel_output = ob_get_clean();
-
-    $context['carousel_output'] = $carousel_output;
-}
-
-// Rendre le template front-page.twig
-Timber::render('front-page.twig', $context);
+Timber::render( array( 'front-page.twig' ), $context );
